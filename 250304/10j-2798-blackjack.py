e088cdc3955ad489ebabe8fc2d91b00f71ee0250
Joey <pjhch1009@gmail.com>
2025-03-10 23:20:58 +0900

    250310 study 02

--- 250304/10j-2798-blackjack.py
+++ 250304/10j-2798-blackjack.py
@@ -21,3 +21,25 @@ visited = [0] * N
 max_v = 0
 black(0)
 print(max_v)
+
+#-----------------------------------------------------------
+def black(i, num):
+    global max_v
+    if i == 3:
+        sum_cards = sum(picked)
+        if sum_cards > M:
+            return
+        max_v = max(max_v, sum_cards)
+        return
+    else:
+        for j in range(num + 1, N):
+            picked[i] = cards[j]
+            black(i+1, j)
+            
+N, M = list(map(int, input().split()))
+cards = list(map(int, input().split()))
+picked = [0] * 3
+# visited = [0] * N
+max_v = 0
+black(0, -1)
+print(max_v)
